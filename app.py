fd360f2d40eef5091200a676b24cacc40d118c31
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 23:02:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from pycaret.classification import load_model, predict_model
 
+# โหลดข้อมูลดิบ
+with open("dataset.csv", "r", encoding="utf-8") as f:
+    first_line = f.readline()
+
+if ";" in first_line:
+    df_raw = pd.read_csv("dataset.csv", sep=";", engine="python")
+else:
+    df_raw = pd.read_csv("dataset.csv", engine="python")
+
+model = load_model("model_programming")
+
+app = dash.Dash(__name__)

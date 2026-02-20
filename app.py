bf64693db059635055e0bf30ea49b9c28f795257
natthapon3090 <garfield11kp@gmail.com>
2026-02-20 23:24:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from dash import Dash, dcc, html, Input, Output
-import dash_bootstrap_components as dbc
 import plotly.express as px
-import plotly.graph_objects as go
 import pandas as pd
 
 # ===============================
@@ -9,21 +7,7 @@ import pandas as pd
 # ===============================
 
 df = pd.read_csv("nvidia_stock.csv")
+
 df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
 df = df.dropna()
-
-# ===============================
-# APP SETUP (Dark Theme)
-# ===============================
-
-app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app.title = "NVIDIA Advanced Dashboard"
-
-# ===============================
-# KPI CALCULATION
-# ===============================
-
-latest_close = df["Close"].iloc[-1]
-previous_close = df["Close"].iloc[-2]
-change_percent = ((latest_close - previous_close) / previous_close) * 100
-latest_volume = df["Volume"].iloc[-1]
+df = df.sort_values("Date")

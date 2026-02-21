0fcb63d38f2a5254f29f1fce4f35b974b74e0a83
natthapon3090 <garfield11kp@gmail.com>
2026-02-21 23:22:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from pycaret.classification import load_model, predict_model
 
-# ===============================
-# LOAD DATA
-# ===============================
+# โหลดข้อมูลดิบ
+with open("dataset.csv", "r", encoding="utf-8") as f:
+    first_line = f.readline()
 
-df = pd.read_csv("nvidia_stock.csv")
+if ";" in first_line:
+    df_raw = pd.read_csv("dataset.csv", sep=";", engine="python")
+else:
+    df_raw = pd.read_csv("dataset.csv", engine="python")
 
-df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
-df = df.dropna()
-df = df.sort_values("Date")
+model = load_model("model_programming")
+
+app = dash.Dash(__name__)
+
+# ---------- Slider block ----------
+def slider_block(label, id_name, min_v, max_v, default):
+    return html.Div([
+        html.Label(label, style={"fontWeight":"bold","fontSize":"18px"}),
+
+        html.Div(id=f"{id_name}_value",
+                 style={
+                     "fontSize":"22px",
+                     "color":"#ffd700",
+                     "marginBottom":"5px"
+                 }),
+
+        dcc.Slider(
+            min=min_v,
+            max=max_v,
+            step=1,
+            value=default,
+            id=id_name,
+            marks={i: str(i) for i in range(min_v, max_v+1)},
+            tooltip={"placement": "bottom", "always_visible": True}
+        ),
+        html.Br()
+    ])
